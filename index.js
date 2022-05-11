669018f6920bd6f5abb599a3eb4ef6b93de307b0
Eric  boyer <ericboyer@mbp-de-eric.lan>
2022-05-11 16:29:18 +0200

    marvel-backend

---
+++ index.js
@@ -0,0 +1,68 @@
+const express = require("express");
+const formidable = require("express-formidable");
+const cors = require("cors");
+const axios = require("axios");
+const moongoose = require("mongoose");
+const morgan = require("morgan");
+
+const app = express();
+app.use(cors());
+app.use(formidable());
+app.use(morgan("dev"));
+
+const ApiKey = "G2BCbDAcAmkGfwrY";
+
+app.get("/comics", async (req, res) => {
+  try {
+    let limit = 100;
+    let page = 1;
+    if (req.query.page < 1) {
+      page = 1;
+    } else {
+      page = req.query.page;
+    }
+
+    let skip = (page - 1) * limit;
+
+    let title;
+    if (req.query.title) {
+      title = req.query.title;
+    }
+
+    const response = await axios.get(
+      `https://lereacteur-marvel-api.herokuapp.com/comics?apiKey=${ApiKey}&limit=${limit}&page=${page}&skip=${skip}&title=${title}`
+    );
+    res.json(response.data);
+  } catch (error) {
+    console.log(error);
+  }
+});
+app.get(" /comics/:characterId", async (req, res) => {
+  const charactereId = req.params.characterId;
+  const response = await axios.get(
+    `https://lereacteur-marvel-api.herokuapp.com/comics/${charactereId}?apiKey=${ApiKey}`
+  );
+  res.json(response.data);
+});
+app.get("/characters", async (req, res) => {
+  const response = await axios.get(
+    `https://lereacteur-marvel-api.herokuapp.com/characters?apiKey=${ApiKey}`
+  );
+  res.json(response.data);
+});
+app.get("/character/:characterId", async (req, res) => {
+  const id = req.params.characterId;
+  const response = await axios.get(
+    `https://lereacteur-marvel-api.herokuapp.com/character/${id}?apiKey=${ApiKey}`
+  );
+  res.json(response.data);
+});
+
+console.log(ApiKey);
+app.all("*", function (req, res) {
+  res.json({ message: "Page not found" });
+});
+
+app.listen(4001, () => {
+  console.log("Server has started");
+});
